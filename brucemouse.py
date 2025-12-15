9bdf420dad4a60897040d6925f4ad1200e0e9442
patriq128 <121152391+patriq128@users.noreply.github.com>
2025-12-15 17:08:25 +0100

    Add files via upload

--- brucemouse.py
+++ brucemouse.py
@@ -133,6 +133,48 @@ d) Everything
             process()
 # ------------------------------------------------------------------------------------------------------------------------------------- 
 
+# Math
+# ------------------------------------------------------------------------------------------------------------------------------------- 
+    def math():
+            print("\033[2J\033[H", end="")
+            print("""\033[32m .d8888b.           888                   888          888    d8b                   
+d88P  Y88b          888                   888          888    Y8P                   
+888    888          888                   888          888                          
+888         8888b.  888  .d8888b 888  888 888  8888b.  888888 888  .d88b.  88888b.  
+888            "88b 888 d88P"    888  888 888     "88b 888    888 d88""88b 888 "88b 
+888    888 .d888888 888 888      888  888 888 .d888888 888    888 888  888 888  888 
+Y88b  d88P 888  888 888 Y88b.    Y88b 888 888 888  888 Y88b.  888 Y88..88P 888  888 
+ "Y8888P"  "Y888888 888  "Y8888P  "Y88888 888 "Y888888  "Y888 888  "Y88P"  888  888 
+                                                                                    
+                                                                                    
+                                                                                    """)
+            print("""░█▀▄░█▀▄░█░█░█▀▀░█▀▀
+░█▀▄░█▀▄░█░█░█░░░█▀▀
+░▀▀░░▀░▀░▀▀▀░▀▀▀░▀▀▀""")
+            print("\033[0m---------------------------------------------------------------------------------------------------------")
+            
+            world = input("Type the passworld:")
+        
+            chars = 0
+
+            has_lower = any(c.islower() for c in world)
+            has_upper = any(c.isupper() for c in world)
+            has_digit = any(c.isdigit() for c in world)
+            has_symbol = any(c in string.punctuation for c in world)
+
+            if has_lower:
+                chars += len(string.ascii_lowercase)
+            if has_upper:
+                chars += len(string.ascii_uppercase)
+            if has_digit:
+                chars += len(string.digits)
+            if has_symbol:
+                chars += len(string.punctuation)
+
+            total = chars ** len(world)
+            print(f"Possibilitys: {total:,}")
+# ------------------------------------------------------------------------------------------------------------------------------------- 
+
 # Help
 # ------------------------------------------------------------------------------------------------------------------------------------- 
     def help():
@@ -233,28 +275,6 @@ d) Config""")
             stop()
             quit()
 
-    def math():
-            world = input("Type the passworld:")
-        
-            chars = 0
-
-            has_lower = any(c.islower() for c in world)
-            has_upper = any(c.isupper() for c in world)
-            has_digit = any(c.isdigit() for c in world)
-            has_symbol = any(c in string.punctuation for c in world)
-
-            if has_lower:
-                chars += len(string.ascii_lowercase)
-            if has_upper:
-                chars += len(string.ascii_uppercase)
-            if has_digit:
-                chars += len(string.digits)
-            if has_symbol:
-                chars += len(string.punctuation)
-
-            total = chars ** len(world)
-            print(f"Possibilitys: {total:,}")
-
     def choosebruce():
         #Asking :3
         print("What type of bruce you want?")
@@ -269,22 +289,6 @@ d) Config""")
             keyboard_bruce()
         
         elif present == "2":
-            print("\033[2J\033[H", end="")
-            print("""\033[32m .d8888b.           888                   888          888    d8b                   
-d88P  Y88b          888                   888          888    Y8P                   
-888    888          888                   888          888                          
-888         8888b.  888  .d8888b 888  888 888  8888b.  888888 888  .d88b.  88888b.  
-888            "88b 888 d88P"    888  888 888     "88b 888    888 d88""88b 888 "88b 
-888    888 .d888888 888 888      888  888 888 .d888888 888    888 888  888 888  888 
-Y88b  d88P 888  888 888 Y88b.    Y88b 888 888 888  888 Y88b.  888 Y88..88P 888  888 
- "Y8888P"  "Y888888 888  "Y8888P  "Y88888 888 "Y888888  "Y888 888  "Y88P"  888  888 
-                                                                                    
-                                                                                    
-                                                                                    """)
-            print("""░█▀▄░█▀▄░█░█░█▀▀░█▀▀
-░█▀▄░█▀▄░█░█░█░░░█▀▀
-░▀▀░░▀░▀░▀▀▀░▀▀▀░▀▀▀""")
-            print("\033[0m---------------------------------------------------------------------------------------------------------")
             math()
         
         elif present == "3":
