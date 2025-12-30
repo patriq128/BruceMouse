dd1bf3d616aa7de3c952a93ff323a2b6704af308
patriq128 <121152391+patriq128@users.noreply.github.com>
2025-12-30 18:55:43 +0100

    Add files via upload

---
+++ Tools/custom_bruce.py
@@ -0,0 +1,17 @@
+import itertools
+import string
+import time
+import json
+
+from setup import term, timecount, stop, device_type
+
+def custom_bruce():
+    print("\033[2J\033[H", end="")
+    print("""\033[32m Custom Bruce \033[0m""") # !Dont forget change NAME!
+    print("\033[0m---------------------------------------------------------------------------------------------------------")
+    print("""a) Show save files
+b) Run save files
+c) New file""")
+
+def custom_program():
+    print("nothing")
